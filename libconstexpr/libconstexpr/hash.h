41c34756505176d2e10238d9c468b7ec06b672f9
Alex van Vliet <alex.van-vliet@epita.fr>
2023-03-22 10:23:59 +0100

    feat: prevent hashing pointers

--- libconstexpr/libconstexpr/hash.h
+++ libconstexpr/libconstexpr/hash.h
@@ -17,6 +17,14 @@ namespace libconstexpr {
     struct hash<std::nullptr_t> {
         constexpr std::size_t operator()(std::nullptr_t) { return 0; }
     };
+
+    template <typename T>
+    struct hash<T*> {
+        // Reinterpreting a pointer to a std::size_t at compile-time is
+        // forbidden both with reinterpret_cast and bit_cast. To prevent
+        // confusion when using constexpr containers, no implementation
+        // for hashing a pointer is provided.
+    };
 } // namespace libconstexpr
 
 #endif // LIBCONSTEXPR_HASH_H
