c32297e539a282568e63b069db201f339b79869b
Alex van Vliet <alex.van-vliet@epita.fr>
2023-03-22 11:17:56 +0100

    feat: add hash for strings

--- libconstexpr/libconstexpr/hash.h
+++ libconstexpr/libconstexpr/hash.h
@@ -1,7 +1,9 @@
 #ifndef LIBCONSTEXPR_HASH_H
 #define LIBCONSTEXPR_HASH_H
 
+#include <string>
 #include <type_traits>
+#include "fnv_hash.h"
 
 namespace libconstexpr {
     template <typename T>
@@ -24,6 +26,13 @@ namespace libconstexpr {
         // forbidden both with reinterpret_cast and bit_cast. To prevent
         // confusion when using constexpr containers, no implementation
         // for hashing a pointer is provided.
+    };
+
+    template <>
+    struct hash<std::string> {
+        constexpr std::size_t operator()(const std::string& s) {
+            return fnv1a_hash_bytes(s.cbegin(), s.cend());
+        }
     };
 } // namespace libconstexpr
 
--- libconstexpr/tests/hash.cpp
+++ libconstexpr/tests/hash.cpp
@@ -1,6 +1,7 @@
 #include <libconstexpr/hash.h>
 
 #include <cstddef>
+#include <string>
 #include <catch2/catch_test_macros.hpp>
 #include <type_traits>
 
@@ -51,6 +52,27 @@ SCENARIO("values can be hashed", "[libconstexpr]") {
             constexpr auto hash_i = libconstexpr::hash<std::nullptr_t>{}(i);
 
             THEN("its hash is 0") { STATIC_REQUIRE(hash_i == 0); }
+        }
+    }
+
+    GIVEN("Two strings") {
+        constexpr char i[6]{"hello"};
+        constexpr char j[5]{"test"};
+
+        WHEN("their hash is computed") {
+            STATIC_REQUIRE(
+                std::is_same_v<decltype(libconstexpr::hash<std::string>{}(
+                                   std::string(i, i + sizeof(i) - 1))),
+                               std::size_t>);
+
+            constexpr auto hash_i = libconstexpr::hash<std::string>{}(
+                std::string(i, i + sizeof(i) - 1));
+            constexpr auto hash_j = libconstexpr::hash<std::string>{}(
+                std::string(j, j + sizeof(j) - 1));
+
+            THEN("their hash is different") {
+                STATIC_REQUIRE(hash_i != hash_j);
+            }
         }
     }
 }
