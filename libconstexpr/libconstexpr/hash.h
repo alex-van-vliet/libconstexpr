591eda9f2b9db33de09264584601b570dc8c3e48
Alex van Vliet <alex.van-vliet@epita.fr>
2023-03-22 10:00:07 +0100

    feat: add hash for integral types

--- libconstexpr/libconstexpr/hash.h
+++ libconstexpr/libconstexpr/hash.h
@@ -1,5 +1,17 @@
 #ifndef LIBCONSTEXPR_HASH_H
 #define LIBCONSTEXPR_HASH_H
 
+#include <type_traits>
 
-#endif //LIBCONSTEXPR_HASH_H
+namespace libconstexpr {
+    template <typename T>
+    struct hash;
+
+    template <typename T>
+    requires(std::is_integral_v<T>)
+    struct hash<T> {
+        constexpr std::size_t operator()(T t) { return t; }
+    };
+} // namespace libconstexpr
+
+#endif // LIBCONSTEXPR_HASH_H
--- libconstexpr/tests/hash.cpp
+++ libconstexpr/tests/hash.cpp
@@ -1,3 +1,42 @@
 #include <libconstexpr/hash.h>
 
+#include <cstddef>
 #include <catch2/catch_test_macros.hpp>
+#include <type_traits>
+
+SCENARIO("values can be hashed", "[libconstexpr]") {
+    GIVEN("Two integers") {
+        const int i{3};
+        const int j{4};
+
+        WHEN("their hash is computed") {
+            STATIC_REQUIRE(
+                std::is_same_v<decltype(libconstexpr::hash<int>{}(i)),
+                               std::size_t>);
+
+            constexpr auto hash_i = libconstexpr::hash<int>{}(i);
+            constexpr auto hash_j = libconstexpr::hash<int>{}(j);
+
+            THEN("their hash is different") {
+                STATIC_REQUIRE(hash_i != hash_j);
+            }
+        }
+    }
+    GIVEN("Two std::size_t") {
+        const std::size_t i{6};
+        const std::size_t j{10};
+
+        WHEN("their hash is computed") {
+            STATIC_REQUIRE(
+                std::is_same_v<decltype(libconstexpr::hash<std::size_t>{}(i)),
+                               std::size_t>);
+
+            constexpr auto hash_i = libconstexpr::hash<std::size_t>{}(i);
+            constexpr auto hash_j = libconstexpr::hash<std::size_t>{}(j);
+
+            THEN("their hash is different") {
+                STATIC_REQUIRE(hash_i != hash_j);
+            }
+        }
+    }
+}
